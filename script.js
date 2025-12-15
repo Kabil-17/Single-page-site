1f043a2af12f2826ee23a6e913d714e59b29f586
Kabil-17 <kabiland44kgm@gmail.com>
2025-12-15 18:08:15 +0530

    checking for mobile

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let totalDrunk = 0;
 let reminderInterval;
 let goal = 0;
+let percent;
 const water = document.querySelector('.s');
 const weight = document.querySelector('.kg');
+const timerInput = document.querySelector('.timer');
+const reminderStatus = document.querySelector('.reminder-status');
+
 
 //localStorage data
 window.addEventListener("load", () => {
@@ -13,8 +17,8 @@ window.addEventListener("load", () => {
     if (savedGoal && savedTotal) {
         goal = Number(savedGoal);
         totalDrunk = Number(savedTotal);
-        water.style.width = savedFill;
-        document.querySelector('.water_needed').innerHTML = `${goal - totalDrunk}/${goal}  ml`;
+        updateFill();
+        updateText();
         document.querySelector('.bt-input').classList.add('show');
         document.querySelector('.remainder').classList.add('show');
     }
@@ -31,8 +35,12 @@ buttons.forEach(button => {
             totalDrunk -= val;
         }
         updateFill();
-        document.querySelector('.water_needed').innerHTML = totalDrunk + ' ml';;
-        if (totalDrunk === 0 && goal) alert('You have completed your water intake for today 🎉');
+        updateText();
+        if (totalDrunk === 0 && goal) {
+            showMessage("🎉 Daily goal completed! Great job!");
+            document.querySelector('.water_needed').innerHTML = '0 ml';
+            document.querySelector('.percent').textContent = " ";
+        }
         saveData();
     });
 });
@@ -40,16 +48,21 @@ buttons.forEach(button => {
 // Set Reminder
 const setRemainder = document.querySelector('.set');
 setRemainder.addEventListener("click", () => {
-    let time = Number(document.querySelector('.timer').value) * 60000;
+    let time = Number(timerInput.value) * 60000;
 
-    if (!time || totalDrunk <= 0) {
-        alert("Enter valid time or you already completed your water intake!");
+    if (!time || time <= 0 || totalDrunk <= 0) {
+        showMessage("Set a valid reminder time!");
         return;
     }
     clearInterval(reminderInterval);
+
     reminderInterval = setInterval(() => {
-        if (totalDrunk > 0) alert(`Drink more ${totalDrunk} ml to complete for this day`);
+        if (totalDrunk > 0) showMessage(`💧 Drink ${totalDrunk} ml to reach your goal`);
     }, time);
+    timerInput.disabled = true;
+    setRemainder.disabled = true;
+    reminderStatus.textContent = `You'll be reminded every ${timerInput.value} min`;
+    showMessage('Reminder Set Successfully');
 });
 
 // Reset Button
@@ -64,16 +77,24 @@ resetBtn.addEventListener("click", () => {
     totalDrunk = 0;
     document.querySelector('.kg').value = "";
     document.querySelector('.timer').value = "";
-    document.querySelector('.water_needed').innerHTML = "0 ml";
+    document.querySelector('.percent').textContent = " ";
+    updateText();
+    
     localStorage.removeItem("goal");
     localStorage.removeItem("totalDrunk");
     localStorage.removeItem("fill");
-    localStorage.removeItem("weight")
-});
+    localStorage.removeItem("weight");
 
-function calculate() {
+    timerInput.disabled = false;
+    setRemainder.disabled = false;
+    calculate.disabled = false;
+    reminderStatus.textContent = "";
+
+});
+const calculate = document.querySelector('.cal');
+calculate.addEventListener("click",()=> {
     if (!weight.value || isNaN(weight.value)) {
-        alert("Enter your weight!");
+        showMessage("Please enter your weight!");
         return;
     }
     clearInterval(reminderInterval);
@@ -88,25 +109,27 @@ function calculate() {
     goal = Math.floor((Number(weight.value) * 0.033) * 1000);
     totalDrunk = goal;
 
-    document.querySelector('.water_needed').innerHTML = `0/${goal}  ml`;
+    updateText();
     weight.value = '';
     saveData();
-}
+    calculate.disabled = true;
+});
 
 //progress bar
 function updateFill() {
     if (!goal) return;
 
     let consumed = goal - totalDrunk;
-    let percent = Math.floor((consumed / goal) * 100);
+    percent = Math.floor((consumed / goal) * 100);
+    document.querySelector('.percent').textContent = percent + "%";
 
-    if(totalDrunk<=0) water.style.width = 0 + "%";
+    if (totalDrunk <= 0) water.style.width = 0 + "%";
     else water.style.width = percent + "%";
 
-    if(percent > 0 && percent<=30) water.style.backgroundColor= "#ff0000";
-    else if(percent > 30 && percent <=60) water.style.backgroundColor = "#ffa500";
-    else if(percent > 60 && percent <= 80) water.style.backgroundColor = "#cefe31";
-    else water.style.backgroundColor = "#07ce07";
+    if (percent > 0 && percent <= 30) water.style.backgroundColor = "#ff0000";
+    else if (percent > 30 && percent <= 60) water.style.backgroundColor = "#ffa500";
+    else if (percent > 60 && percent <= 80) water.style.backgroundColor = "#cefe31";
+    else water.style.backgroundColor = "#22C55E";
 }
 
 //localStorage
@@ -114,4 +137,25 @@ function saveData() {
     localStorage.setItem("goal", goal);
     localStorage.setItem("totalDrunk", totalDrunk);
     localStorage.setItem("fill", water.style.width);
+}
+
+//text update
+function updateText() {
+    if (!goal) {
+        document.querySelector('.water_needed').innerHTML = "0 ml";
+        return;
+    }
+    const consumed = goal - totalDrunk;
+    document.querySelector('.water_needed').innerHTML = `${consumed} / ${goal} ml`;
+}
+
+//toast
+const message = document.querySelector('.message');
+
+function showMessage(text, duration = 2500) {
+    message.textContent = text;
+    message.classList.add('show');
+    setTimeout(() => {
+        message.classList.remove('show')
+    }, duration);
 }
